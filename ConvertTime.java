ae1305091d39e7389501a3292280d529adb4893c
Afterburnz <1289840@learn.vsb.bc.ca>
2026-09-16 17:21:11 -0700

    Add files via upload

--- ConvertTime.java
+++ ConvertTime.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
 public class ConvertTime {
-	public static void main(String[] args) {
-		final int SEC_PER_HOUR = 3600;
-		final int MIN_PER_HOUR = 60;
-		Scanner in = new Scanner(System.in);
-		System.out.print("Enter an amount of seconds: ");
-		int seconds = in.nextInt();
-		int hours = (int) (seconds/SEC_PER_HOUR);
-		int minutes = (seconds/SEC_PER_HOUR) % SEC_PER_MIN;
-		int rem = hours % SEC_PER_MIN;
-		System.out.printf("%d seconds = %d hours, %d minutes, and %d seconds", seconds, hours, minutes, rem);
-	}		
+    public static void main(String[] args) {
+        final int SEC_PER_HOUR = 3600;
+        final int SEC_PER_MIN = 60;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter an amount of seconds: ");
+        int seconds = in.nextInt();
+        int hours = seconds / SEC_PER_HOUR;
+        int minutes = (seconds % SEC_PER_HOUR) / SEC_PER_MIN;
+        int rem = seconds % SEC_PER_MIN;
+        System.out.printf("%d seconds = %d hours, %d minutes, and %d seconds",
+                seconds, hours, minutes, rem);
+    }
 }
